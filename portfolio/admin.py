d7bafa30ec0fb8a1e8d29b952cf3084f0705d712
ermarti-syan <elizabet3000@mail.ru>
2020-11-04 15:54:40 +0300

    fix admin panel

--- portfolio/admin.py
+++ portfolio/admin.py
@@ -4,8 +4,8 @@ from portfolio.models import Portfolio
 
 
 class Portfolios(admin.ModelAdmin):
-    list_display = ('id', 'name', 'stock_weights', 'stock_tickers', 'stock_exchanges')
-    fields = ['id', 'name', 'stock_weights', 'stock_tickers', 'stock_exchanges']
+    list_display = ('name', 'stock_weights', 'stock_tickers', 'stock_exchanges')
+    fields = ['name', 'stock_weights', 'stock_tickers', 'stock_exchanges']
     list_display_links = ('name',)
     view_on_site = True
 
