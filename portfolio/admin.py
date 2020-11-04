af2acb8424f6238e78d1b6e16a5045535720f2f2
ermarti-syan <elizabet3000@mail.ru>
2020-11-04 15:47:46 +0300

    add admin panel for portfolio

---
+++ portfolio/admin.py
@@ -0,0 +1,13 @@
+from django.contrib import admin
+
+from portfolio.models import Portfolio
+
+
+class Portfolios(admin.ModelAdmin):
+    list_display = ('id', 'name', 'stock_weights', 'stock_tickers', 'stock_exchanges')
+    fields = ['id', 'name', 'stock_weights', 'stock_tickers', 'stock_exchanges']
+    list_display_links = ('name',)
+    view_on_site = True
+
+
+admin.site.register(Portfolios, Portfolio)
